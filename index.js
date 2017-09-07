ba408299a6794fd53d448903e8dda5da799ba4ad
Manoela Silverio Gomes <manoela_sgomes@hotmail.com>
2017-09-07 11:42:48 -0300

    Primeiro commit - Estrutura

---
+++ index.js
@@ -0,0 +1,29 @@
+// Requerimentos
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const podem_ajudar = require('./data/podem_ajudar.json');
+const precisam_ajuda = require('./data/precisam_ajuda.json');
+
+// Inicializa app web
+const app = express();
+
+// Configuração Handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Middlewares
+app.use(session({secret: 'mf_money'}));
+app.use(express.static('assets'));
+app.use(bodyParser.urlencoded());
+
+// Rotas
+app.get('/', (request, response) => {
+    response.render('index', {home: true});
+});
+
+// Gera servidor
+app.listen(3000, () => {
+    console.log('Servidor inicializado na porta 3000');
+});
