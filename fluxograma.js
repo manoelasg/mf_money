567515edf369519854fa096fac5815e04ba8683b
Alex Monteiro <alexcda@gmail.com>
2017-09-26 09:12:32 -0300

    fluxograma.js
    Fiz um esboço inicial do Desenho do Projeto

---
+++ fluxograma.js
@@ -0,0 +1,29 @@
+/* Esquema do projeto:
+
+Login do Usuário Credor:
+    Nome
+    email
+    telefone
+    profissão
+    Renda
+    CPF
+    Rate (Estipulado automaticamente pelo sistema)
+    Garantia / Fiador
+
+Login do Usuário Tomador de Emprestimo
+    Nome
+    email
+    telefone
+    profissão
+    Renda
+    CPF
+    Rate (Estipulado automaticamente pelo sistema)
+
+  - Usuário tomador de emprestimo vai fazer input do valor que deseja pegar emprestado
+  - Usuário indica em quantas parcelas deseja pagar
+  - O sistema estipula autamenticamente a taxa de juros levando em conta a renda, o rate,
+  o valor e o número de parcelas que o usuário tomador de emprestimo deseja pagar
+  - O sistema calculará a taxa de juros que o usuário credor receberá por ter emprestado o valor
+  - O sistema calculará a taxa de juros que o usuário tomador de emprestimo pagará pelo empréstimo
+  realizado o emprestimo
+   */
